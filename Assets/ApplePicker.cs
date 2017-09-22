924cc7e324d5b9af2e9dcb4f599a734f4c4807e6
Albert Peng <albertpeng96@yahoo.com>
2017-09-22 03:26:31 -0400

    Finished Game

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class ApplePicker : MonoBehaviour {
 
@@ -9,13 +10,20 @@ public class ApplePicker : MonoBehaviour {
 	public float            basketBottomY = -14f;
 	public float            basketSpacingY = 2f;
 
+	public List<GameObject> basketList;
+
 	// Use this for initialization
 	void Start () {
+
+		basketList = new List<GameObject>();
+
 		for (int i=0; i<numBaskets; i++) {
 			GameObject tBasketGO = Instantiate( basketPrefab ) as GameObject;
 			Vector3 pos = Vector3.zero;
 			pos.y = basketBottomY + ( basketSpacingY * i );
 			tBasketGO.transform.position = pos;
+
+			basketList.Add( tBasketGO );                                    // 3
 		}
 	}
 
@@ -23,4 +31,29 @@ public class ApplePicker : MonoBehaviour {
 	void Update () {
 
 	}
-}
+
+	public void AppleDestroyed() {                                          // 2
+		//// Destroy all of the falling Apples
+		GameObject[] tAppleArray=GameObject.FindGameObjectsWithTag("Apple");// 3
+		foreach (GameObject tGO in tAppleArray) {
+			Destroy (tGO);
+
+		}
+
+			//// Destroy one of the Baskets
+			// Get the index of the last Basket in basketList
+			int basketIndex = basketList.Count-1;
+			// Get a reference to that Basket GameObject
+			GameObject tBasketGO = basketList[basketIndex];
+			// Remove the Basket from the List and destroy the GameObject
+			basketList.RemoveAt( basketIndex );
+			Destroy( tBasketGO );
+
+		// Restart the game, which doesn't affect HighScore.Score
+		if ( basketList.Count == 0 ) {
+			Application.LoadLevel( "_Scene_0" );
+		}
+		}
+
+
+	}
---
+++ Assets/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+	static public int    score = 1000;
+
+	void Awake() {                                                          // 1
+		// If the ApplePickerHighScore already exists, read it
+		if (PlayerPrefs.HasKey("ApplePickerHighScore")) {                   // 2
+			score = PlayerPrefs.GetInt("ApplePickerHighScore");
+		}
+		// Assign the high score to ApplePickerHighScore
+		PlayerPrefs.SetInt("ApplePickerHighScore", score);                  // 3
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TextMesh gt = this.GetComponent<TextMesh>();
+		gt.text = "High Score: "+score;
+		// Update ApplePickerHighScore in PlayerPrefs if necessary
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore")) {           // 4
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+}
